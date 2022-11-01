4fe13f6b28b889df4d0eac656b5335779af51302
PandY <pk.dmitriev@gmail.com>
2022-11-01 22:32:52 +0300

    Попытка написать проект №2. №1 бесследно погиб, жаль.

---
+++ src/Avito/Hosts/Avito.Api/Controllers/AdController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Avito.Api.Controllers;
+
+/// <summary>
+/// Работа с контроллером объявлений
+/// </summary>
+
+[ApiController]
+[Route("v1/[controller]")]
+public class AdController : ControllerBase
+{
+    private readonly ILogger<AdController> _logger;
+
+    public AdController()
+    {
+    }
+
+    /// <summary>
+    /// Добавить новое объявление
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return await Task.FromResult(Ok());
+    }
+    
+    /// <summary>
+    /// Удалить созданное тобой объявление
+    /// </summary>
+    /// <returns></returns>
+    [HttpDelete]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        return await Task.FromResult(Ok());
+    }
+
+    /// <summary>
+    /// Тут что-то будет
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IActionResult> GetAsync()
+    {
+        return await Task.FromResult(Ok());
+    }
+}
